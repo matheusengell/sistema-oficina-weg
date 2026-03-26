8fc3d17cf89222051d1c58061cd69f890a59defa
matheus engel <matheusengelfl@gmail.com>
2026-03-26 16:57:04 -0300

    feat: cria os métodos de turma(nao finaliza)

--- src/main/java/com/weg/oficina/recuperacao/controller/TurmaController.java
+++ src/main/java/com/weg/oficina/recuperacao/controller/TurmaController.java
@@ -1,4 +1,27 @@
 package com.weg.oficina.recuperacao.controller;
 
+import com.weg.oficina.recuperacao.dto.turma.TurmaRequestDto;
+import com.weg.oficina.recuperacao.dto.turma.TurmaResponseDto;
+import com.weg.oficina.recuperacao.service.TurmaService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/turma")
+@RequiredArgsConstructor
 public class TurmaController {
+
+    private final TurmaService service;
+
+    @PostMapping
+    public TurmaResponseDto criar(@RequestBody TurmaRequestDto requestDto){
+        return service.criar(requestDto);
+    }
+
+    @GetMapping
+    public List<TurmaResponseDto> listar(){
+        return service.turmas();
+    }
 }
--- src/main/java/com/weg/oficina/recuperacao/service/TurmaService.java
+++ src/main/java/com/weg/oficina/recuperacao/service/TurmaService.java
@@ -1,10 +1,32 @@
 package com.weg.oficina.recuperacao.service;
 
+import com.weg.oficina.recuperacao.dto.turma.TurmaRequestDto;
+import com.weg.oficina.recuperacao.dto.turma.TurmaResponseDto;
+import com.weg.oficina.recuperacao.mapper.TurmaMapper;
+import com.weg.oficina.recuperacao.model.Turma;
+import com.weg.oficina.recuperacao.repository.TurmaRepository;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class TurmaService {
+
+    private final TurmaRepository repository;
+    private final TurmaMapper mapper;
+
+    public TurmaResponseDto criar(TurmaRequestDto requestDto){
+        Turma turma = mapper.paraEntidade(requestDto);
+        repository.save(turma);
+        return mapper.paraDto(turma);
+    }
+
+    public List<TurmaResponseDto> turmas(){
+        return repository.findAll().stream()
+                .map(mapper::paraDto)
+                .toList();
+    }
 }
