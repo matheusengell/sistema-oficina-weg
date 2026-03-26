9821ddb3a33c3af13a7118050efe2cbc2e19cb83
matheus engel <matheusengelfl@gmail.com>
2026-03-26 16:39:11 -0300

    feat: cria os métodos de professor

--- src/main/java/com/weg/oficina/recuperacao/controller/ProfessorController.java
+++ src/main/java/com/weg/oficina/recuperacao/controller/ProfessorController.java
@@ -1,4 +1,28 @@
 package com.weg.oficina.recuperacao.controller;
 
+import com.weg.oficina.recuperacao.dto.professor.ProfessorRequestDto;
+import com.weg.oficina.recuperacao.dto.professor.ProfessorResponseDto;
+import com.weg.oficina.recuperacao.service.ProfessorService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/professor")
+@RequiredArgsConstructor
 public class ProfessorController {
+
+    private final ProfessorService service;
+
+    @PostMapping
+    public ProfessorResponseDto criar(@PathVariable ProfessorRequestDto requestDto){
+        return service.criar(requestDto);
+    }
+
+    @GetMapping
+    public List<ProfessorResponseDto> listarProfessores(){
+        return service.listarProfessores();
+    }
+
 }
--- src/main/java/com/weg/oficina/recuperacao/service/ProfessorService.java
+++ src/main/java/com/weg/oficina/recuperacao/service/ProfessorService.java
@@ -1,10 +1,34 @@
 package com.weg.oficina.recuperacao.service;
 
+import com.weg.oficina.recuperacao.dto.professor.ProfessorRequestDto;
+import com.weg.oficina.recuperacao.dto.professor.ProfessorResponseDto;
+import com.weg.oficina.recuperacao.mapper.ProfessorMapper;
+import com.weg.oficina.recuperacao.model.Professor;
+import com.weg.oficina.recuperacao.repository.ProfessorRepository;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class ProfessorService {
+
+    private final ProfessorRepository repository;
+    private final ProfessorMapper mapper;
+
+    public ProfessorResponseDto criar(ProfessorRequestDto requestDto){
+        Professor professor = mapper.paraEntidade(requestDto);
+        repository.save(professor);
+        return mapper.paraDto(professor);
+    }
+
+    public List<ProfessorResponseDto> listarProfessores(){
+        List<Professor> professores = repository.findAll();
+        return professores.stream()
+                .map(mapper::paraDto)
+                .toList();
+    }
+
 }
